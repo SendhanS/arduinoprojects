deda27009480e43803555fddac4e1323623b5cb3
Sendhan S <113227030+SendhanS@users.noreply.github.com>
2023-09-21 00:31:32 +0530

    Add files via upload

---
+++ automatic plant watering system.ino
@@ -0,0 +1,69 @@
+
+#define pump1 3
+
+const int sensor_pin = A0;
+const int water_l =A1;
+
+void pump();
+
+
+void setup() 
+{
+pinMode(pump1 ,OUTPUT);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+float moisture_percentage;
+int sensor_analog;
+int water_analog;
+float water_level;
+sensor_analog = analogRead(sensor_pin);
+water_analog = analogRead(water_l);
+moisture_percentage = ( 100 - ( (sensor_analog/1023.00)* 100 ) );
+if(moisture_percentage < 50)
+{
+Serial.print("pumping water");
+pump();
+delay(5000);
+sensor_analog= analogRead(sensor_pin);
+moisture_percentage = ( 100 - ( (sensor_analog/1023.00)* 100 ) );
+  
+}
+else if(moisture_percentage < 50)
+{
+Serial.print("Pumping water");
+pump();
+delay(1000);
+sensor_analog= analogRead(sensor_pin);
+moisture_percentage = ( 100 - ( (sensor_analog/1023.00)* 100 ) );
+}
+else
+{  
+}
+water_level = ( 100 - ( (water_analog/1023.00)* 100 ) );
+Serial.print("Water Level:=");
+Serial.print(water_level);
+Serial.print("%\n\n");
+Serial.print("Moisture percentage =");
+Serial.print(moisture_percentage);
+Serial.print("%\n\n");
+if(water_level < 20)
+{
+  Serial.print("Tank level LOW");
+}
+delay(1000);
+
+}
+void pump()
+{
+  digitalWrite(pump1,HIGH);
+  delay(15000);
+  digitalWrite(pump1,LOW);
+  }
+
+
+
+
---
+++ temperature_based_exhaust_cooling_system.ino
@@ -0,0 +1,31 @@
+const int lm35Pin = A0;      // LM35 temperature sensor connected to A0
+const int relayControlPin = 2;  // Control pin for the relay module
+
+// Set the temperature threshold for turning on the fan
+const float temperatureThreshold = 30.0; // Adjust as needed
+
+void setup() {
+  pinMode(relayControlPin, OUTPUT);
+  digitalWrite(relayControlPin, LOW); // Initially turn off the relay (fan)
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read temperature from LM35 sensor
+  float temperature = analogRead(lm35Pin) * 0.48875; // Convert analog reading to degrees Celsius
+
+  // Print temperature to serial monitor (for debugging)
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  // Check if temperature exceeds the threshold to turn on the fan
+  if (temperature >= temperatureThreshold) {
+    digitalWrite(relayControlPin, HIGH); // Turn on the relay (fan)
+  } else {
+    digitalWrite(relayControlPin, LOW); // Turn off the relay (fan)
+  }
+
+  delay(1000); // Delay for 1 second (adjust as needed)
+}
+
